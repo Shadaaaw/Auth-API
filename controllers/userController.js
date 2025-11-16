dc4b9e7b9a630d5dc7764110260c42c1c522a91d
Shadaaaw <yshadaaaw@gmail.com>
2025-11-16 20:48:36 +0100

    verifyEmail func

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,6 +7,7 @@ const httpStatusText = require('../utils/httpStatusText');
 const generateJWT = require('../utils/generateJWT');
 const mongoose = require('mongoose');
 const sendEmail = require('../config/email');
+const verificationToken = require('../models/verificationToken');
 
 const register = asyncWrapper(async (req, res, next) => {
     const {name, email, password} = req.body;
@@ -115,10 +116,55 @@ const login = asyncWrapper(async (req, res, next) => {
 
     const token = await generateJWT({id: user._id, email : user.email, verified : user.verified});
 
-
     res.status(200).json({
         status: httpStatusText.SUCCESS,
         message: 'User logged in successfully',
+        data: {token}
+    })
+});
+
+const verifyEmail = asyncWrapper(async (req, res, next) => {
+    const {code} = req.body;
+    if(!code){
+        return next(appError.create(
+            'Must provide code',
+            httpStatusText.ERROR,
+            400
+        ))
+    }
+    const lastVerifyToken = await verificationToken.findOne({userId: req.user.id, type:'email'}).sort({createdAt:-1});
+    if(!lastVerifyToken){
+        return next(appError.create(
+            'No verification code found',
+            httpStatusText.ERROR,
+            400
+        ))
+    }
+    if (lastVerifyToken.expiresAt < Date.now()){
+        return next(appError.create(
+            'Code invalid or expired',
+            httpStatusText.ERROR,
+            400
+        ))
+    }
+    const matchedCode = await bcrypt.compare(code, lastVerifyToken.code);
+    if(!matchedCode){
+        return next(appError.create(
+            'Code invalid or expired',
+            httpStatusText.ERROR,
+            400
+        ))
+    }
+    const user = await User.findByIdAndUpdate(
+        req.user.id,
+        {verified: true},
+        { new: true } //return the new document
+    )
+    const token = await generateJWT({id: user._id, email : user.email, verified : user.verified});
+    await VerificationToken.deleteMany({userId : user._id, type:'email'});
+    res.status(200).json({
+        status: httpStatusText.SUCCESS,
+        message: 'User verified',
         data: {token}
     })
 });
@@ -127,5 +173,6 @@ const login = asyncWrapper(async (req, res, next) => {
 module.exports = {
     register,
     sendVerificationCode,
-    login
+    login,
+    verifyEmail
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const { verifyToken} = require('../middlewares/authMiddleware');
-const {register, login, sendVerificationCode} = require('../controllers/userController');
+const {register, login, sendVerificationCode, verifyEmail} = require('../controllers/userController');
 
 router.post("/login", login);
 router.post("/register", register);
 router.post("/send-verification-code",verifyToken, sendVerificationCode);
+router.patch("/verify-email", verifyToken, verifyEmail);
 
 module.exports = router;
 
